51a2bcd3b56f6fc909581aa63a977fae1c80db9a
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-13 19:56:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-
+const dns=require("dns")
+dns.serServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017//courseTrackerDB");
+        await mongoose.connect("mongodb+srv://tammirsidevika05_db_user:<db_password>@cluster0.7ywegpc.mongodb.net/?appName=Cluster0");
         console.log("Database Connected Successfully");
     } catch (error) {
         console.log("Database Connection Failed");
